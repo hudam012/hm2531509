5ad9686db9fef260b4a16ea3fcfd67163bd2b34c
hm2531509 <hudam012@yahoo.com>
2014-06-25 21:08:09 -0700

    Signed-off-by: hm2531509 <hudam012@yahoo.com>

---
+++ Hmwk/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:  main.cpp      
+ * Author: Huda Milbes
+ *Created on June 25, 2014, 2:25 PM
+ *Purpose: CSC 5 Assignment 1 
+ */ 
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"Hello World"<<endl;
+    
+
+
+    return 0;
+}
+
