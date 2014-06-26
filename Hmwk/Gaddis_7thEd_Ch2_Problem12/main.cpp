807f413be9e7c286ad8f9ab250ea71f3e6a3ac81
hm2531509 <hudam012@yahoo.com>
2014-06-25 21:05:30 -0700

    Signed-off-by: hm2531509 <hudam012@yahoo.com>

---
+++ Hmwk/Gaddis_7thEd_Ch2_Problem12/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 25, 2014, 8:51 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"   *"<<endl;
+    cout<<"  ***"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"*******"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"  ***"<<endl;
+    cout<<"   *"<<endl;
+
+    return 0;
+}
+
