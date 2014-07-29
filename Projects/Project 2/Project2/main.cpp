a4fee8e0cec380e8df1dfaf7597248866cc4cba7
hudam012 <hudam012@yahoo.com>
2014-07-29 12:22:00 -0700

    Signed-off-by: hudam012 <hudam012@yahoo.com>

--- Projects/Project 2/Project2/main.cpp
+++ Projects/Project 2/Project2/main.cpp
@@ -3,14 +3,13 @@
  * Author: Huda Milbes
  * Project 2
  *
- * Created on July 29, 2014, 9:52 AM
+ * Created on July 29, 2014, 11:55 AM
  */
 
-#include <iostream>
 #include <cstdlib>
-#include <ctime>
+#include <iostream>
 #include <string>
-#include <cmath>
+#include <ctime>
 
 using namespace std;
 
@@ -23,15 +22,8 @@ void genieRoom (int & bananas, int & oranges);
 void pictureRoom ();
 
 
-
 int main(int argc, char** argv) {
     
-    
-    cout << "Hello! Do you want to play a game? "<<endl;
-    cout << "Yes? Alright then ... \n"<<endl;
-    cout << "Please state the following information, \n"<<endl;
-   
-   
    string monstername = " ";
    
    int bananas = 5;
@@ -48,74 +40,91 @@ int main(int argc, char** argv) {
    
    string lname = " "; //Last Name
    
-   float age; //Age
-   
    string monster = " ";
    
+   float age;
    
-   cout << "Please enter your first name: ";
+   //Enter Player's Information
+   //Input first name
+   cout << "First Name: ";
+   
    cin >> fname;
+   
    cout << endl;
    
-   cout << "Please enter you last name: ";
+   
+   //Input last name
+   cout << "Last Name: ";
+   
    cin >> lname;
-   cout << endl;
    
-   cout << "Please state how old you are: ";
+   cout << endl; 
+   
+   
+   //Input age
+   cout << "Age: ";
+   
    cin >> age;
+   
    cout << endl;
    
-   
-   if (age >12)
-  
-   {      
 
-   cout << "Name the monster you are most afraid of: ";
+   cout << "Monster: ";
+   
    cin >> monster;
+   
    cout << endl;
    
-   //We use for random orientation
+   
+   //Used for random
    srand (500);
+   
    srand (time(0)); 
+   
 
    while (doornumber != "exit")
    {
+       cout << fname << " " << lname << ", you are in a room with 4 doors.";
        
-       cout << fname << " " << lname << ", you are in a room with 4 doors." << endl;
+       cout << endl;
+       
        cout << "You are carrying " << bananas << " bananas and " << oranges 
        << " oranges." << endl;
+       
        cout << endl;
        
-    
+       //Enter 1 out of 4 possible directions
+       //North, South, East, West
        cout << "Pick a door to enter by typing the direction it is " 
        << "in (N/E/S/W): ";
-       cin >> doornumber;
-       cout << endl;
        
+       cin >> doornumber;
+       
+       cout << endl;
     
-    
+      
        while( (doornumber != "W") && (doornumber != "E") && (doornumber != "N") 
            && (doornumber != "S") )
+           
        {
            
-           
+           //Enter 1 out of 4 possible directions
+           //North, South, East, West
            cout << "Pick a door to enter by typing the direction it is in "
            << "(N/E/S/W): ";
            cin >> doornumber;
            cout << endl;
            
-           
        }
-   }
        
-   
- 
-       //Also used for random orientation
+       
        //Sets a random orientation
        orientation = rand() % 2;
+       
     
-       //First possible orientation
+       //Possible Orientation 1
        if(orientation == 0)
+           
        {
         
            if(doornumber == "N")
@@ -137,9 +146,11 @@ int main(int argc, char** argv) {
        }
        
        
-       //Second possible orientation
+       
+       //Possible Orientation 2
        else if(orientation == 1)
        {
+           
            if(doornumber == "N")
            {
                doornumber = "exit";
@@ -152,69 +163,76 @@ int main(int argc, char** argv) {
            {
                genieRoom (bananas, oranges);
            }
-           else if (doornumber == "W")
-           
+           else if(doornumber == "W")
+           {
                pictureRoom ();
-           
+           }
        }
+       
     
     
+   }
+
+   //End Game
+   //Calculate Score
+   cout << "You found the exit!" << endl;
    
-   
-   //Ends the game
-   cout<<"You found the exit!"<<endl;
    score = bananas + oranges;
    
-   //Output Score
    cout << "Your score is " << score << " (" << bananas << " bananas and " 
    << oranges << " oranges)." << endl;  
-   cout << "Goodbye now!!!" << endl;
-   }
    
-   else if (age <=12) 
-   {
-       
-       cout << "I'm sorry! You are way too young. Come back in a year or two.";
-       cout << endl; 
-       
-   }
+   cout << "Bye bye!!!" << endl;
+
 
     return 0;
 }
 
-//You lose all your bananas and oranges in this room
-//Score is equal to 0
+
+//Possible room
+//Points reset to zero
 void monsterRoom (int & bananas, int & oranges, string monster)
 {
    bananas = 0;
+   
    oranges = 0;
    
    cout << "WATCH OUT!!" << endl;
+   
    cout << monster << " attacks you and steals all of your bananas and " 
    << "oranges." << endl;
+   
    cout << endl;
 }
 
-//You are granted more bananas and oranges in this room
-//3 points gets added to score
+
+//Possible room
+//Three points added to current score
 void genieRoom (int & bananas, int & oranges)
 {
    bananas = bananas + 2;
+   
    oranges = oranges + 1;
    
    cout << "!!POOF!!" << endl;
+   
    cout << "A genie pops out and grants you 2 bananas and 1 orange.";
    cout << endl;
+   
 }
 
-//Picture room
+
+//Possible Room
+//Picture Room
 void pictureRoom ()
 {
    cout << "You found a picture!" << endl;
+   
    drawPicture();
 }
 
-//Draws picture of a house
+
+//Displays picture of a house
 void drawPicture()
 {
    cout << "        _--~~--_" << endl;
@@ -230,4 +248,3 @@ void drawPicture()
    cout << "   ~~              ~~" << endl;
    cout << endl;
 }
-
